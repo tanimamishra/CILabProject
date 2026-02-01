e9d35f17965776ff32def88ebf473a6af1b32790
tanimamishra <tanimamishra13@gmail.com>
2026-02-01 21:55:27 +0530

    Add Calculator class with add and subtract methods

--- src/main/java/com/muj/ci/Calculator.java
+++ src/main/java/com/muj/ci/Calculator.java
@@ -1,1 +1,13 @@
 
+package com.muj.ci;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
